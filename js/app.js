62df5f8449f867a83da7b51bb8fe526c925eec6a
TroyAWilson <38123313+TroyAWilson@users.noreply.github.com>
2018-05-17 14:02:08 -0700

    Keyboard working
    The keyboard now reacts with the hidden phrase and reveals the letters if they're clicked

--- js/app.js
+++ js/app.js
@@ -1,29 +1,65 @@
 const startButton = document.getElementsByClassName('btn__reset');
 const qwerty = document.getElementById('qwerty');
 const phrase = document.getElementById('phrase');
+const ul = document.getElementsByTagName('ul')[0];
+const qwertyBtns = document.getElementsByTagName('button');
 let missed = 0;
 
-let phrases = [/* 5 phrases that I'll think of later */];
+let phrases = [
+  "someday your prince charming will come",
+  "people say nothing is impossible",
+  "just remember if the world didnt suck we would all fall off",
+  "cheer up the worst is yet to come",
+  "he who laughs last didnt get it"
+];
 
 
 
-function getRandomPhraseAsArray (arr){
-    //takes an phrase from the phrases array and divides it into its own array
-    //of single letters
-    //Make sure that this function is reusable
-    //getRandomPhraseAsArray(phrases);
+function getRandomPhraseAsArray (phrases){
+
+      let randomNumber = Math.floor(Math.random()*5);
+
+      let randomPhrase = phrases[randomNumber];
+    //takes a phrase from the phrases array and divides it into its own array of single letters
+      var arrayOfStrings = randomPhrase.split("");
+        return arrayOfStrings;
 };
 
-function addPhraseToDisplay(arr){
+function addPhraseToDisplay(){
   // do stuff any arr that is passed in, and add to `#phrase ul`
+  const phraseArray = getRandomPhraseAsArray(phrases);
+
+  for(let i = 0; i < phraseArray.length; i++)
+    {
+      let li = document.createElement('li');
+      li.innerHTML = phraseArray[i];
+      ul.appendChild(li).style.display ="none";
+      li.classList.add('phraseClass');
+    }
 };
 
 //const phraseArray = getRandomPhraseAsArray(phrases);
-//addPhrasetoDisplay(phraseArray);
+//addPhraseToDisplay(phraseArray);
 
-function checkLetter(){
+function checkLetter(btnText){
+    console.log(btnText);
+    let phraseList = document.getElementsByClassName('phraseClass');
+
+    for (let i = 0; i < phraseList.length; i++)
+    {
+      if (btnText === phraseList[i].innerHTML)
+      {
+        phraseList[i].style.display ="inline-block";
+      }
+    };
+
+};
 
 
+for (let i = 0; i < qwertyBtns.length; i++){
+  qwertyBtns[i].addEventListener("click", () =>{
+    checkLetter(qwertyBtns[i].textContent);
+  });
 };
 
 //event listener for the keyboard
@@ -35,10 +71,14 @@ function checkLetter(){
 
 
 //create a checkWin funciton
-
+function checkWin(){
+  //if missed count = 5
+      //display game loss
+};
 
 
 //Game Start
 startButton[0].addEventListener("click", () =>{
+  addPhraseToDisplay();
   document.getElementById('overlay').style.display = 'none';
 });
